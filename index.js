a63c504e1083f794c4504aa4b8a95f68f0736c55
Stepan-a11y <kostko52@gmail.com>
2023-05-17 18:41:10 +0300

    add request

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const connectDB = require('./connectDB')
+const Guests = require('./schemas/guestsSchema');
 
 const app = express()
+const router = express.Router();
 
+app.use(cors()); 
+app.use(bodyParser.json());
 const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+router.post( '/newguest', (req, res) => {
+    let newGuest = new Guests({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        status: req.body.status,
+    });
+    
+    try {
+        newGuest.save();
+        res.json(newGuest);    
+    } catch (err) {
+        res.json({success: false, msg: "not add"});
+    }
+});
+
+app.use('/api', router)
 
 mongoose.connect(connectDB.wedding, { useNewUrlParser: true, useUnifiedTopology: true })
 
